dfe7723810b7e9525a3c2b17623b4fb18ab9d0c2
ggNo1 <g@1998.ws>
2019-02-26 20:41:37 +0800

    first commit

---
+++ src/main/java/com/gg3083/jenkins/cnotroller/TestController.java
@@ -0,0 +1,18 @@
+package com.gg3083.jenkins.cnotroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author GG
+ * @date 2019/2/26 20:39
+ */
+@RestController
+public class TestController {
+
+
+    @GetMapping("/")
+    public String test() {
+        return "master";
+    }
+}
