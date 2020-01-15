35db8b98aae1d9d6c26f95863d34c6f578ec1852
gimi <g@1998.ws>
2020-01-15 10:51:37 +0800

    3

--- src/main/java/com/gg3083/jenkins/controller/TestController.java
+++ src/main/java/com/gg3083/jenkins/controller/TestController.java
@@ -1,6 +1,7 @@
-package com.gg3083.jenkins.cnotroller;
+package com.gg3083.jenkins.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
